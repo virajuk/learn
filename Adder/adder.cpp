7ce5503ffc1397d9dc1c36b177956884e7e46784
virajuk <virajudayakumara@gmail.com>
2021-10-31 01:20:26 -0400

    first commit

---
+++ Adder/adder.cpp
@@ -0,0 +1,6 @@
+#include "adder.hpp"
+
+float add(float a, float b)
+{
+    return a + b;
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <adder.hpp>
+
+int main(int argc, char const *argv[])
+{
+    std::cout << "HOOOOO" << std::endl;
+
+    std::cout << add(72.1f, 73.8f) << std::endl;
+
+    return 0;
+}
